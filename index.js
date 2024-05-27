753d2614228a8dcff59fff728dd4f8b7760e7afe
Luis cespedes <luis.cespedes@valposystems.com>
2024-05-27 12:25:15 -0400

    morgan

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+
 
 const app = express();
 const port = 8000;
 app.use(cors());
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.json({
